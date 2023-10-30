bed5c19a8c5c3854257888051e137a44c6103dcf
KristinaTar <83270933+KristinaTar@users.noreply.github.com>
2023-10-30 09:40:22 +0200

    Revert "added navbar inside component"
    This reverts commit cefa34a79ef10a45db3c2d9ed85c0714a8364ad3.

---
+++ src/components/SliderNavBar/SliderNavBar.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import SliderNavBarStyles from '@/components/SliderNavBar/SliderNavBar.styled';
+
+const SliderNavBar = ({ activeIndex = 0, swiper }) => (
+  <SliderNavBarStyles
+    activeindex={activeIndex}
+    slidesnumber={swiper?.slides?.length || 0}
+    slidesperview={swiper?.params?.slidesPerView || 0}
+  >
+    <div className="progressBar" />
+    <div className="buttonsContainer">
+      <div
+        className="button prev"
+        onClick={() => {
+          swiper.slidePrev();
+        }}
+      >
+        <i className="icon-angle-down iconLeft" />
+      </div>
+      <div
+        className="button next"
+        onClick={() => {
+          swiper.slideNext();
+        }}
+      >
+        <i className="icon-angle-down iconRight" />
+      </div>
+    </div>
+  </SliderNavBarStyles>
+);
+
+export default SliderNavBar;
+
+SliderNavBar.propTypes = {
+  activeIndex: PropTypes.number,
+  swiper: PropTypes.shape({}),
+};
+
+SliderNavBar.defaultProps = {
+  activeIndex: 0,
+  swiper: {},
+};
