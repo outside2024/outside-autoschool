0094d5ac8a01b2f4e51c8f02d7e4c400b7b0a360
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2023-10-30 13:32:25 +0200

    buton

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -5,6 +5,12 @@ export const theme = {
     black: '#231E20',
     primary: '#76C045',
     white: '#FFFFFF',
+    button: {
+      green: '#76C045',
+      green30: 'rgba(118, 192, 69, 0.3)',
+      green70: 'rgba(118, 192, 69, 0.7)',
+      transparent: 'transparent',
+    },
   },
   gradients: {},
 };
