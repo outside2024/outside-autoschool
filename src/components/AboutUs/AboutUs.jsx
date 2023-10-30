5008a52baa17d40ca43e0e0c4a584bdb6bf3ebbf
KristinaTar <83270933+KristinaTar@users.noreply.github.com>
2023-10-30 11:13:08 +0200

    prettier

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -14,45 +14,45 @@ const AboutUs = () => {
 
   return (
     <StyledAboutUs className="contentContainer">
-        <div className="contentWrapper">
-          <h2 className="typoColorBlack typoTitleSecondary">{t('aboutUs.title')}</h2>
-          <h3 className="typoColorBlack typoSubtitle">{t('aboutUs.subtitle')}</h3>
-          <div className="swiperContainer">
-            <Swiper
-              slidesPerView="auto"
-              spaceBetween={24}
-              breakpoints={{
-                0: { slidesPerView: 2, spaceBetween: 16 },
-                1000: { slidesPerView: 3, spaceBetween: 24 },
-                1440: { slidesPerView: 5, spaceBetween: 24 },
-              }}
-              onSwiper={setSwiper}
-              onSlideChange={(_swiper) => {
-                setActiveIndex(_swiper.activeIndex);
-              }}
-            >
-              {aboutUsPhotoSlider.map((card) => (
-                <SwiperSlide key={card.id} className="swiperSlide">
-                  <div>
-                    <Image
-                      src={card.photo}
-                      width={229}
-                      height={304}
-                      quality={85}
-                      alt="teacher photo"
-                      className="aboutImage"
-                      priority
-                    />
-                    <div className="aboutName">{t(`${card.name}`)}</div>
-                    <div className="aboutText">{t(`${card.position}`)}</div>
-                    <div className="aboutText">{card.experience}</div>
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-            <SliderNavBar activeIndex={activeIndex} swiper={swiper} />
-          </div>
+      <div className="contentWrapper">
+        <h2 className="typoColorBlack typoTitleSecondary">{t('aboutUs.title')}</h2>
+        <h3 className="typoColorBlack typoSubtitle">{t('aboutUs.subtitle')}</h3>
+        <div className="swiperContainer">
+          <Swiper
+            slidesPerView="auto"
+            spaceBetween={24}
+            breakpoints={{
+              0: { slidesPerView: 2, spaceBetween: 16 },
+              1000: { slidesPerView: 3, spaceBetween: 24 },
+              1440: { slidesPerView: 5, spaceBetween: 24 },
+            }}
+            onSwiper={setSwiper}
+            onSlideChange={(_swiper) => {
+              setActiveIndex(_swiper.activeIndex);
+            }}
+          >
+            {aboutUsPhotoSlider.map((card) => (
+              <SwiperSlide key={card.id} className="swiperSlide">
+                <div>
+                  <Image
+                    src={card.photo}
+                    width={229}
+                    height={304}
+                    quality={85}
+                    alt="teacher photo"
+                    className="aboutImage"
+                    priority
+                  />
+                  <div className="aboutName">{t(`${card.name}`)}</div>
+                  <div className="aboutText">{t(`${card.position}`)}</div>
+                  <div className="aboutText">{card.experience}</div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <SliderNavBar activeIndex={activeIndex} swiper={swiper} />
         </div>
+      </div>
     </StyledAboutUs>
   );
 };
