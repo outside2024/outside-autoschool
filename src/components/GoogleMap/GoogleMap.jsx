2a2c34fbe3640b4a2055489f94a9cba8197c3b79
KristinaTar <83270933+KristinaTar@users.noreply.github.com>
2023-10-31 10:47:57 +0200

    working on addresses block

--- src/components/GoogleMap/GoogleMap.jsx
+++ src/components/GoogleMap/GoogleMap.jsx
@@ -1,32 +1,45 @@
 'use client'
 
-import {Map, GoogleApiWrapper, Marker} from "google-maps-react";
-import {markers} from "@/components/GoogleMap/markers";
-import GoogleMapStyled from "@/components/GoogleMap/GoogleMap.styled";
-import {useTranslation} from "next-i18next";
 
-const mapStyles = {
+import {useTranslation} from "next-i18next";
+import {Map, GoogleApiWrapper, Marker} from "google-maps-react";
+import {data} from "@/components/GoogleMap/data";
+import GoogleMapStyled from "@/components/GoogleMap/GoogleMap.styled";
+import useWindowSize from "@/hooks/useWindowSize";
+
+
+
+const desktopMapStyles = {
   width: '100%',
-  height: '50%'
+  height: '718px'
+};
+
+const mobileMapStyles = {
+  width: '100%',
+  height: '288px'
 };
 
 const GoogleMap = () => {
   const { t } = useTranslation();
 
+  const widowSize=useWindowSize();
+
   return (
     <GoogleMapStyled>
       <div className="contentContainer">
         <div className="contentWrapper">
-          <h1>{t('branches.title')}</h1>
+          <h2 className="typoColorBlack typoTitleSecondar branchesTitle">{t('branches.title')}</h2>
         </div>
       </div>
-
-
-
+      <div className="branchesAddress">
+        <h2 className="typoColorBlack typoTitleSecondar">{t('branches.addresses.city')}</h2>
+        <div>+38 (098)055-59-99</div>
+        <div>+38 (067)609-75-24</div>
+      </div>
           <Map
             google={window.google}
             zoom={12}
-            style={mapStyles}
+            style={widowSize.width >= 720 ? desktopMapStyles: mobileMapStyles}
             initialCenter={
               {
                 lat: 48.4647,
@@ -34,15 +47,13 @@ const GoogleMap = () => {
               }
             }
           >
-            {markers.map(marker => (
+            {data.map(marker => (
               <Marker
                 key={marker.id}
                 position={marker.position}
               />
             ))}
-
           </Map>
-
 
     </GoogleMapStyled>
   )
--- src/components/GoogleMap/GoogleMap.styled.js
+++ src/components/GoogleMap/GoogleMap.styled.js
@@ -1,7 +1,33 @@
 import styled from 'styled-components';
+import theme from "@/styles/theme";
+
 
 
 const GoogleMapStyled = styled(`div`)`
+  padding-top: 36px;
+
+
+
+  
+  .branches{
+    &Title{
+      padding-bottom: 36px;
+    }
+    &Address {
+      width: 366px;
+      height:718px;
+      background-color: ${theme.colors.bg.white};
+      position: absolute;
+      z-index: 10;
+      right: 93px;
+      padding-top: 29px;
+      padding-left: 40px;
+
+      @media only screen and (max-width: 720px) {
+        display: none;
+      }
+    }
+  }
 
 
   `;
