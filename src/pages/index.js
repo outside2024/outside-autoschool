ed9818f06b45db4aef23bcc0c8c3577c74b5abf0
KristinaTar <83270933+KristinaTar@users.noreply.github.com>
2023-11-02 16:52:23 +0200

    added changes from development

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import RootLayout from '@/layouts/RootLayout';
 import Hero from '@/components/Hero';
-import AboutUs from '@/components/AboutUs/AboutUs';
 import GoogleMap from '@/components/GoogleMap/GoogleMap';
-import HowToGetStarted from '@/components/HowToGetStarted';
 import AboutUs from '@/components/AboutUs/AboutUs';
 import FAQ from '@/components/FAQ';
 import { HeroTypes } from '@/components/Hero/Hero';
@@ -17,10 +15,8 @@ const Home = () => {
     <RootLayout>
       <Hero heroType={HeroTypes.PRIMARY} />
       <AboutUs />
-      <FAQ />
-      <HowToGetStarted />
-      <AboutUs />
       <GoogleMap activeBranch="dnipro" />
+      <FAQ />
     </RootLayout>
   );
 };
